ae6eae37fedf56527d6a4b9218f9fd5277d47ebc
Milos <milosmisicbg@gmail.com>
2018-08-15 20:48:53 +0200

    First

---
+++ src/main/java/com/home/tidy/TidyApplication.java
@@ -0,0 +1,12 @@
+package com.home.tidy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TidyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TidyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/home/tidy/TidyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.home.tidy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TidyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
